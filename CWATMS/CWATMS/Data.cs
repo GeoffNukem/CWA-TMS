3ba12d4fe5b6e30616587a4415bf8efe5f214b2d
Geoffrey Grundell <Geoff17391@gmail.com>
2012-10-16 22:55:31 +0100

    Project Solution Start up
    Added Data & Lecturer class. Also added a start up of the Unit Testing
    for the Lecturer class.

---
+++ CWATMS/CWATMS/Data.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CWATMS
+{
+    public abstract class Data
+    {
+        protected String m_name;
+        protected String m_label;
+        protected Color m_colour;
+
+        /// <summary>
+        /// Data Constructor
+        /// </summary>
+        /// <param name="name">Name of data</param>
+        /// <param name="label">Abbriviation of name</param>
+        /// <param name="colour">Colour of data for display</param>
+        public Data(String name, String label, Color colour)
+        {
+            m_name = name;
+            m_label = label;
+            m_colour = colour;
+            
+        }
+
+        /// <summary>
+        /// Name of data
+        /// </summary>
+        public String Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+
+        /// <summary>
+        /// Abbriviation of Name
+        /// </summary>
+        public String Label
+        {
+            get { return m_label; }
+            set { m_label = value; }
+        }
+
+        /// <summary>
+        /// Colour of data for display
+        /// </summary>
+        public Color Colour
+        {
+            get { return m_colour; }
+            set { m_colour = value; }
+        }
+    }
+}
---
+++ CWATMS/UnitTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CWATMS;
+using System.Drawing;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestLecturerName()
+        {
+            Lecturer lec1 = new Lecturer("Geoff", "Grundell", 7, "GG", Color.Red);
+            Assert.AreEqual("Geoff", lec1.Name, false);
+        }
+
+        [TestMethod]
+        public void TestLecturerSurname()
+        {
+            Lecturer lec1 = new Lecturer("Geoff", "Grundell", 7, "GG", Color.Red);
+            Assert.AreEqual("Grundell", lec1.Surname, false);
+        }
+
+        [TestMethod]
+        public void TestLecturerFullName()
+        {
+            Lecturer lec1 = new Lecturer("Geoff", "Grundell", 7, "GG", Color.Red);
+            Assert.AreEqual("Geoff Grundell", lec1.FullName, false);
+        }
+
+        [TestMethod]
+        public void TestLecturerInitials()
+        {
+            Lecturer lec1 = new Lecturer("Geoff", "Grundell", 7, "GG", Color.Red);
+            Assert.AreEqual("GG", lec1.Initials, false);
+        }
+    }
+}
