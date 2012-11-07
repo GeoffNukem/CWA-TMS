a961c4549d18db0d71f44a0b17fc2b631a484935
Geoffrey Grundell <Geoff17391@gmail.com>
2012-11-07 12:00:31 +0000

    Implemented the Add Room Unit Tests

--- CWATMS/CWATMS.UnitTest/RoomTest.cs
+++ CWATMS/CWATMS.UnitTest/RoomTest.cs
@@ -9,13 +9,25 @@ namespace CWATMS.UnitTest
     public class RoomTest
     {
         [TestMethod]
-        public void RoomConstructorTest()
+        public void Room_Constructor_Test()
         {
             Room r = new Room("Network Lab", "Y106", Color.Blue, 20);
             Assert.AreEqual("Network Lab", r.Name);
             Assert.AreEqual("Y106", r.Label);
             Assert.AreEqual<Color>(Color.Blue, r.Colour);
             Assert.AreEqual<int>(20, r.Capacity);
+        }
+
+        [TestMethod]
+        public void Room_Add_Test()
+        {
+            Room r1 = new Room("Room1", "L1", Color.Blue, 20);
+            Room r2 = new Room("Room2", "L2", Color.Blue, 19);
+            Room r3 = new Room("Room3", "L3", Color.Blue, 17);
+
+            DataCollection.Instance.Add(r1);
+            DataCollection.Instance.Add(r2);
+            DataCollection.Instance.Add(r3);
         }
     }
 }
