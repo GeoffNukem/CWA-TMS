bb62c246508b07c85c70ca3a1fab42a35dc84d01
Geoffrey Grundell <Geoff17391@gmail.com>
2012-11-07 12:00:10 +0000

    Implemented the Add Lecturer Unit Tests.

--- CWATMS/CWATMS.UnitTest/LecturerTest.cs
+++ CWATMS/CWATMS.UnitTest/LecturerTest.cs
@@ -10,7 +10,7 @@ namespace CWATMS.UnitTest
     {
         
         [TestMethod]
-        public void LecturerConstructorTest()
+        public void Lecturer_Constructor()
         {
             Lecturer l = new Lecturer("Joe", "Bloggs", 14, "JB", Color.Blue);
             Assert.AreEqual("Joe", l.Name);
@@ -20,6 +20,24 @@ namespace CWATMS.UnitTest
             Assert.AreEqual("Joe Bloggs", l.FullName);
             Assert.AreEqual<int>(14, l.HoursPerWeek);
             Assert.AreEqual<Color>(Color.Blue, l.Colour);
+        }
+
+        [TestMethod]
+        public void Lecturer_Add()
+        {
+            Lecturer l = new Lecturer("Joe", "Bloggs", 14, "JB", Color.Blue);
+            Lecturer l2 = new Lecturer("John", "Smith", 32, "JS1", Color.Blue);
+            Lecturer l3 = new Lecturer("Fred", "Flintstone", 50, "FF", Color.Blue);
+
+            DataCollection.Instance.Add(l);
+            Assert.AreEqual<Lecturer>(l, DataCollection.Instance.Lecturers[0], "Lecturer1 not found at 0");
+
+            DataCollection.Instance.Add(l2);
+            Assert.AreEqual<Lecturer>(l2, DataCollection.Instance.Lecturers[1], "Lecturer2 not found at 1");
+
+            DataCollection.Instance.Insert(l3, 1);
+            Assert.AreEqual<Lecturer>(l3, DataCollection.Instance.Lecturers[1], "Lecturer3 not found at 1");
+            Assert.AreEqual<Lecturer>(l2, DataCollection.Instance.Lecturers[2], "Lecturer2 not found at 2");
         }
         
     }
