0ada2b7f26babbcfc3b02604d1d7c34df34c841e
Geoffrey Grundell <Geoff17391@gmail.com>
2012-11-13 10:01:53 +0000

    Added comments & set constuctor to private

--- CWATMS/CWATMS/Data/DataCollection.cs
+++ CWATMS/CWATMS/Data/DataCollection.cs
@@ -19,7 +19,7 @@ namespace CWATMS
         /// Singleton.
         /// Collection instances of the Data classes: Lecturer, Room, Module & Groups.
         /// </summary>
-        public DataCollection()
+        private DataCollection()
         {
             Lecturers = new List<Lecturer>();
             Rooms = new List<Room>();
@@ -27,26 +27,47 @@ namespace CWATMS
             Groups = new List<Group>();
         }
 
+        /// <summary>
+        /// Add Lecturer
+        /// </summary>
+        /// <param name="lecturer">Lecturer instance to add into list</param>
         public void Add(Lecturer lecturer)
         {
             Lecturers.Add(lecturer);
         }
 
+        /// <summary>
+        /// Add Room
+        /// </summary>
+        /// <param name="room">Room instance to add into list</param>
         public void Add(Room room)
         {
             Rooms.Add(room);
         }
 
+        /// <summary>
+        /// Add Module
+        /// </summary>
+        /// <param name="module">Module instance to add into list</param>
         public void Add(Module module)
         {
             Modules.Add(module);
         }
 
+        /// <summary>
+        /// Add Group
+        /// </summary>
+        /// <param name="group">Group instance to add into list</param>
         public void Add(Group group)
         {
             Groups.Add(group);
         }
 
+        /// <summary>
+        /// Insert Lecturer into the list at a specific Index
+        /// </summary>
+        /// <param name="lecturer"></param>
+        /// <param name="index"></param>
         public void Insert(Lecturer lecturer, int index)
         {
             Lecturers.Insert(index, lecturer);
@@ -71,6 +92,24 @@ namespace CWATMS
         {
             if (Lecturers.Contains(lecturer))
                 Lecturers.Remove(lecturer);
+        }
+
+        public void Remove(Room room)
+        {
+            if (Rooms.Contains(room))
+                Rooms.Remove(room);
+        }
+
+        public void Remove(Module module)
+        {
+            if (Modules.Contains(module))
+                Modules.Remove(module);
+        }
+
+        public void Remove(Group group)
+        {
+            if (Groups.Contains(group))
+                Groups.Remove(group);
         }
 
         public static DataCollection Instance
