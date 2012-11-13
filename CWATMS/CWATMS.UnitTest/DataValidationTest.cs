4975ea6c642043f4c3b50ead34bd7f125889003c
Geoffrey Grundell <Geoff17391@gmail.com>
2012-11-13 10:02:15 +0000

    Added DataValidation & Unit Tests

---
+++ CWATMS/CWATMS.UnitTest/DataValidationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CWATMS.UnitTest
+{
+    [TestClass]
+    public class DataValidationTest
+    {
+        /// <summary>
+        /// Test the ContainsNumbers method in the DataValidation class.
+        /// </summary>
+        [TestMethod]
+        public void Data_ContainsNumbers()
+        {
+            Assert.IsFalse(DataValidation.Instance.ContainsNumbers("ABC"));
+            Assert.IsTrue(DataValidation.Instance.ContainsNumbers("ABC123"));
+        }
+
+        [TestMethod]
+        public void Data_IsInRange()
+        {
+            //  Test 1: 5 is between 0 & 10.
+            Assert.IsTrue(DataValidation.Instance.IsInRange(5, 0, 10), "Failed @ test 1");
+
+            //  Test 2: 3 is between 1 & 3, with lessThanOrEqualTo set to true.
+            Assert.IsTrue(DataValidation.Instance.IsInRange(3, 1, 3, true), "Failed @ test 2");
+
+            //  Test 3: 1 is between 1 & 3, with lessThanOrEqualTo set to true.
+            Assert.IsTrue(DataValidation.Instance.IsInRange(1, 1, 3, true), "Failed @ test 3");
+
+            //  Test 4: 20 is out of range.
+            Assert.IsFalse(DataValidation.Instance.IsInRange(20, 0, 10), "Failed @ test 4");
+
+            //  Test 5: 12 is out of range of 3 & 12 as lessThanOrEqualTo set to false.
+            Assert.IsFalse(DataValidation.Instance.IsInRange(12, 3, 12), "Failed @ test 5");
+            
+        }
+    }
+}
---
+++ CWATMS/CWATMS/Data/DataValidation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWATMS
+{
+    public sealed class DataValidation
+    {
+        private static readonly DataValidation instance = new DataValidation();
+
+        private DataValidation() { }
+
+        /// <summary>
+        /// Checks if a string contains any numbers
+        /// </summary>
+        /// <param name="text">Text to check for numbers</param>
+        /// <returns>Boolean value if the string contains numbers</returns>
+        public bool ContainsNumbers(string text)
+        {
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (char.IsNumber(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Is an Integer within a range.
+        /// </summary>
+        /// <param name="num">Number to check</param>
+        /// <param name="min">Minimum number</param>
+        /// <param name="max">Maximum number</param>
+        /// <param name="lessThanOrEqualTo">Can number equal to the min and max numbers</param>
+        /// <returns>Returns if the number is between the min & max values</returns>
+        public bool IsInRange(int num, int min, int max, bool lessThanOrEqualTo = false)
+        {
+            return lessThanOrEqualTo ? (num >= min && num <= max) : (num > min && num < max);
+        }
+
+        public static DataValidation Instance
+        {
+            get { return instance; }
+        }
+    }
+}
