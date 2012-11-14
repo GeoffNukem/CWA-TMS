d78f9ef90e5b960ac3bf4cd7e839d994744ca0f1
Trisodium <tommysaward@hotmail.com>
2012-11-14 10:58:12 +0000

    Added Export to PDF Form

---
+++ CWATMS/CWATMS/Export.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Diagnostics;
+
+namespace CWATMS
+{
+    public partial class Export : Form
+    {
+        public Export()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string lOL = "List of Lecturers";
+            Document document = new Document();
+            PdfWriter.GetInstance(document, new FileStream(lOL + ".pdf", FileMode.Create));
+            document.Open();
+            Paragraph paragraph = new Paragraph(lOL + System.Environment.NewLine);
+            PdfPTable t1 = new PdfPTable(5);
+
+            PdfPCell cfname = new PdfPCell(new Phrase("First Name"));
+            PdfPCell clname = new PdfPCell(new Phrase("Last Name"));
+            PdfPCell chpw = new PdfPCell(new Phrase("Hours Per Week"));
+            PdfPCell clabel = new PdfPCell(new Phrase("Label"));
+            PdfPCell ccolour = new PdfPCell(new Phrase("Colour"));
+
+            t1.AddCell(cfname);
+            t1.AddCell(clname);
+            t1.AddCell(chpw);
+            t1.AddCell(clabel);
+            t1.AddCell(ccolour);
+
+            foreach (DataGridViewRow rows in dataGridView_List_Lecturers.Rows)
+            {
+                if (Convert.ToBoolean(dataGridView_List_Lecturers.Rows[rows.Index].Cells[2].Value))
+                {
+                    string fname = dataGridView_List_Lecturers.Rows[rows.Index].Cells["First Name"].Value.ToString();
+                    string lname = dataGridView_List_Lecturers.Rows[rows.Index].Cells["Last Name"].Value.ToString();
+                    string hpw = dataGridView_List_Lecturers.Rows[rows.Index].Cells["Hours Per Week"].Value.ToString();
+                    string label = dataGridView_List_Lecturers.Rows[rows.Index].Cells["Label"].Value.ToString();
+                    string colour = dataGridView_List_Lecturers.Rows[rows.Index].Cells["Colour"].Value.ToString();
+
+                    PdfPCell c1 = new PdfPCell(new Phrase(fname));
+                    PdfPCell c2 = new PdfPCell(new Phrase(lname));
+                    PdfPCell c3 = new PdfPCell(new Phrase(hpw));
+                    PdfPCell c4 = new PdfPCell(new Phrase(label));
+                    PdfPCell c5 = new PdfPCell(new Phrase(colour));
+
+                    t1.AddCell(c1);
+                    t1.AddCell(c2);
+                    t1.AddCell(c3);
+                    t1.AddCell(c4);
+                    t1.AddCell(c5);
+                }
+            }
+            document.Add(paragraph);
+            document.Add(t1);
+            document.Close();
+
+            MessageBox.Show(lOL + "has been exported",
+                "Export",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1);
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Document document = new Document();
+            PdfWriter.GetInstance(document, new FileStream("List of Modules.pdf", FileMode.Create));
+            document.Open();
+            Paragraph paragraph = new Paragraph("List of Modules" + System.Environment.NewLine);
+            PdfPTable t1 = new PdfPTable(4);
+
+            PdfPCell cname = new PdfPCell(new Phrase("Name"));
+            PdfPCell ccourselevel = new PdfPCell(new Phrase("Course Level"));
+            PdfPCell clabel = new PdfPCell(new Phrase("Label"));
+            PdfPCell ccolour = new PdfPCell(new Phrase("Colour"));
+
+            t1.AddCell(cname);
+            t1.AddCell(ccourselevel);
+            t1.AddCell(clabel);
+            t1.AddCell(ccolour);
+        }
+
+
+
+        private void List_Room_Export_Click(object sender, EventArgs e)
+        {
+            string name1 = "List of Rooms";
+            Document document = new Document();
+            PdfWriter.GetInstance(document, new FileStream( name1 + ".pdf", FileMode.Create));
+            document.Open();
+            Paragraph paragraph = new Paragraph(name1 + System.Environment.NewLine);
+            PdfPTable t1 = new PdfPTable(4);
+
+            PdfPCell cname = new PdfPCell(new Phrase("Name"));
+            PdfPCell ccapacity = new PdfPCell(new Phrase("Capacity"));
+            PdfPCell clabel = new PdfPCell(new Phrase("Label"));
+            PdfPCell ccolour = new PdfPCell(new Phrase("Colour"));
+
+            t1.AddCell(cname);
+            t1.AddCell(ccapacity);
+            t1.AddCell(clabel);
+            t1.AddCell(ccolour);
+        }
+
+        private void List_Groups_Export_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Timetable_Groups_Export_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
