4d22ff2fd0e5bdfe6092ee4a383f95055f66a012
Geoffrey Grundell <Geoff17391@gmail.com>
2012-11-20 09:36:56 +0000

    Added feature to create Timetable with Drag and Drop. Added DataButton and GridPanel controls. Implemented Drag and Drop. Click and drag from the tabs in the main form and drop into the cells in the timetable.

---
+++ CWATMS/CWATMS/Controls/DataButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CWATMS.Controls
+{
+    public class DataButton : Button
+    {
+        private Lecturer m_lecturer;
+        private Room m_room;
+        private Module m_module;
+        private Group m_group;
+
+        public DataButton(){}
+
+        public Lecturer Lecturer
+        {
+            get { return m_lecturer; }
+            set { m_lecturer = value; }
+        }
+
+        public Room Room
+        {
+            get { return m_room; }
+            set { m_room = value; }
+        }
+
+        public Module Module
+        {
+            get { return m_module; }
+            set { m_module = value; }
+        }
+
+        public Group Group
+        {
+            get { return m_group; }
+            set { m_group = value; }
+        }
+
+
+    }
+}
---
+++ CWATMS/CWATMS/FormTimetable.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace CWATMS
+{
+    partial class FormTimetable
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTimetable));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpTimetable = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblFriday = new System.Windows.Forms.Label();
+            this.lblThursday = new System.Windows.Forms.Label();
+            this.lblWednesday = new System.Windows.Forms.Label();
+            this.lblTuesday = new System.Windows.Forms.Label();
+            this.lblMonday = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.tlsView = new System.Windows.Forms.ToolStrip();
+            this.btnLecturer = new System.Windows.Forms.ToolStripButton();
+            this.btnModule = new System.Windows.Forms.ToolStripButton();
+            this.btnRoom = new System.Windows.Forms.ToolStripButton();
+            this.btnGroup = new System.Windows.Forms.ToolStripButton();
+            this.lblViewBy = new System.Windows.Forms.ToolStripLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tlsView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 83.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.tlpTimetable, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.33334F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(952, 411);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tlpTimetable
+            // 
+            this.tlpTimetable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tlpTimetable.ColumnCount = 9;
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpTimetable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpTimetable.Location = new System.Drawing.Point(164, 74);
+            this.tlpTimetable.Name = "tlpTimetable";
+            this.tlpTimetable.RowCount = 5;
+            this.tlpTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpTimetable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpTimetable.Size = new System.Drawing.Size(783, 332);
+            this.tlpTimetable.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.lblFriday, 0, 4);
+            this.tableLayoutPanel3.Controls.Add(this.lblThursday, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.lblWednesday, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.lblTuesday, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.lblMonday, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(5, 74);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 5;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(151, 332);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // lblFriday
+            // 
+            this.lblFriday.AutoSize = true;
+            this.lblFriday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblFriday.Location = new System.Drawing.Point(5, 266);
+            this.lblFriday.Name = "lblFriday";
+            this.lblFriday.Size = new System.Drawing.Size(141, 64);
+            this.lblFriday.TabIndex = 4;
+            this.lblFriday.Text = "Friday";
+            this.lblFriday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblThursday
+            // 
+            this.lblThursday.AutoSize = true;
+            this.lblThursday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblThursday.Location = new System.Drawing.Point(5, 200);
+            this.lblThursday.Name = "lblThursday";
+            this.lblThursday.Size = new System.Drawing.Size(141, 64);
+            this.lblThursday.TabIndex = 3;
+            this.lblThursday.Text = "Thursday";
+            this.lblThursday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblWednesday
+            // 
+            this.lblWednesday.AutoSize = true;
+            this.lblWednesday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWednesday.Location = new System.Drawing.Point(5, 134);
+            this.lblWednesday.Name = "lblWednesday";
+            this.lblWednesday.Size = new System.Drawing.Size(141, 64);
+            this.lblWednesday.TabIndex = 2;
+            this.lblWednesday.Text = "Wednesday";
+            this.lblWednesday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTuesday
+            // 
+            this.lblTuesday.AutoSize = true;
+            this.lblTuesday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTuesday.Location = new System.Drawing.Point(5, 68);
+            this.lblTuesday.Name = "lblTuesday";
+            this.lblTuesday.Size = new System.Drawing.Size(141, 64);
+            this.lblTuesday.TabIndex = 1;
+            this.lblTuesday.Text = "Tuesday";
+            this.lblTuesday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMonday
+            // 
+            this.lblMonday.AutoSize = true;
+            this.lblMonday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMonday.Location = new System.Drawing.Point(5, 2);
+            this.lblMonday.Name = "lblMonday";
+            this.lblMonday.Size = new System.Drawing.Size(141, 64);
+            this.lblMonday.TabIndex = 0;
+            this.lblMonday.Text = "Monday";
+            this.lblMonday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel4.ColumnCount = 9;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(164, 5);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(783, 61);
+            this.tableLayoutPanel4.TabIndex = 2;
+            // 
+            // tlsView
+            // 
+            this.tlsView.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tlsView.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.tlsView.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnGroup,
+            this.btnRoom,
+            this.btnModule,
+            this.btnLecturer,
+            this.lblViewBy});
+            this.tlsView.Location = new System.Drawing.Point(0, 411);
+            this.tlsView.Name = "tlsView";
+            this.tlsView.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tlsView.Size = new System.Drawing.Size(952, 25);
+            this.tlsView.TabIndex = 1;
+            this.tlsView.Text = "toolStrip1";
+            // 
+            // btnLecturer
+            // 
+            this.btnLecturer.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnLecturer.Image = ((System.Drawing.Image)(resources.GetObject("btnLecturer.Image")));
+            this.btnLecturer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLecturer.Name = "btnLecturer";
+            this.btnLecturer.Size = new System.Drawing.Size(23, 22);
+            this.btnLecturer.Text = "L";
+            this.btnLecturer.ToolTipText = "Lecturer";
+            this.btnLecturer.Click += new System.EventHandler(this.btnLecturer_Click);
+            // 
+            // btnModule
+            // 
+            this.btnModule.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnModule.Image = ((System.Drawing.Image)(resources.GetObject("btnModule.Image")));
+            this.btnModule.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnModule.Name = "btnModule";
+            this.btnModule.Size = new System.Drawing.Size(23, 22);
+            this.btnModule.Text = "S";
+            this.btnModule.ToolTipText = "Subject";
+            this.btnModule.Click += new System.EventHandler(this.btnModule_Click);
+            // 
+            // btnRoom
+            // 
+            this.btnRoom.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnRoom.Image = ((System.Drawing.Image)(resources.GetObject("btnRoom.Image")));
+            this.btnRoom.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRoom.Name = "btnRoom";
+            this.btnRoom.Size = new System.Drawing.Size(23, 22);
+            this.btnRoom.Text = "R";
+            this.btnRoom.ToolTipText = "Room";
+            this.btnRoom.Click += new System.EventHandler(this.btnRoom_Click);
+            // 
+            // btnGroup
+            // 
+            this.btnGroup.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnGroup.Image = ((System.Drawing.Image)(resources.GetObject("btnGroup.Image")));
+            this.btnGroup.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnGroup.Name = "btnGroup";
+            this.btnGroup.Size = new System.Drawing.Size(23, 22);
+            this.btnGroup.Text = "C";
+            this.btnGroup.ToolTipText = "Class";
+            this.btnGroup.Click += new System.EventHandler(this.btnGroup_Click);
+            // 
+            // lblViewBy
+            // 
+            this.lblViewBy.Name = "lblViewBy";
+            this.lblViewBy.Size = new System.Drawing.Size(51, 22);
+            this.lblViewBy.Text = ":View By";
+            // 
+            // FormTimetable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 436);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tlsView);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormTimetable";
+            this.Text = "CWA Timetable Management System";
+            this.Load += new System.EventHandler(this.FormTimetable_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tlsView.ResumeLayout(false);
+            this.tlsView.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tlpTimetable;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label lblFriday;
+        private System.Windows.Forms.Label lblThursday;
+        private System.Windows.Forms.Label lblWednesday;
+        private System.Windows.Forms.Label lblTuesday;
+        private System.Windows.Forms.Label lblMonday;
+        private System.Windows.Forms.ToolStrip tlsView;
+        private System.Windows.Forms.ToolStripButton btnLecturer;
+        private System.Windows.Forms.ToolStripButton btnModule;
+        private System.Windows.Forms.ToolStripButton btnRoom;
+        private System.Windows.Forms.ToolStripButton btnGroup;
+        private System.Windows.Forms.ToolStripLabel lblViewBy;
+
+    }
+}
